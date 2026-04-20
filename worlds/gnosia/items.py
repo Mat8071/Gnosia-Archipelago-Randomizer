7db3a163f2e69dfb67fe28e9d88daaf18eb6d8cd
Mat8071 <61936958+Mat8071@users.noreply.github.com>
2026-04-20 23:21:49 +0200

    Create items.py

---
+++ worlds/gnosia/items.py
@@ -0,0 +1,24 @@
+from BaseClasses import Item, ItemClassification
+
+ITEM_NAME_TO_ID = {
+
+}
+
+DEFAULT_ITEM_CLASSIFICATIONS = {
+
+}
+
+class GnosiaItem(Item):
+    game = "Gnosia"
+
+def get_random_filler_item_name(world: GnosiaWorld) -> str:
+    #TODO: Make this
+    pass
+
+def create_item_with_correct_classification(world: GnosiaWorld, name: str) -> None:
+    #TODO: Make this
+    pass
+
+def create_all_items(world: GnosiaWorld) -> None:
+    #TODO: Make this
+    pass
