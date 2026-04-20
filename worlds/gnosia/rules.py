8dec0570985a74fedec9f931abf228b35938d86d
Mat8071 <61936958+Mat8071@users.noreply.github.com>
2026-04-20 23:16:32 +0200

    Create rules.py

---
+++ worlds/gnosia/rules.py
@@ -0,0 +1,19 @@
+from rule_builder.options import OptionFilter
+from rule_builder.rules import Has, HasAll, Rule
+
+def set_all_rules(world: GnosiaWorld) -> None:
+    set_all_entrance_rules(world)
+    set_all_location_rules(world)
+    set_completion_condition(world)
+
+def set_all_entrance_rules(world: GnosiaWorld) -> None:
+    #TODO: Make this
+    pass
+
+def set_all_location_rules(world: GnosiaWorld) -> None:
+    #TODO: Make this
+    pass
+
+def set_completion_condition(world: GnosiaWorld) -> None:
+    #TOOD: Make this
+    pass
