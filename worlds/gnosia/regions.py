04993c22ff73549195624563207d056cc9c78ebd
Mat8071 <61936958+Mat8071@users.noreply.github.com>
2026-04-20 22:55:45 +0200

    Create regions.py

---
+++ worlds/gnosia/regions.py
@@ -0,0 +1,13 @@
+from BaseClasses import Entrance, Region
+
+def create_and_connect_regions(world: GnosiaWorld) -> None:
+  create_all_regions(world)
+  connect_all_regions(world)
+
+def create_all_regions(world: GnosiaWorld) -> None:
+  #TODO: Make this
+  pass
+
+def connect_all_regions(world: GnosiaWorld) -> None:
+  #TODO: Make this
+  pass
