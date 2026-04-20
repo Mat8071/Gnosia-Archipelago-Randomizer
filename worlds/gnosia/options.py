5feaf4e063d2409ff36db463bf31444ed6ca8e70
Mat8071 <61936958+Mat8071@users.noreply.github.com>
2026-04-20 23:25:17 +0200

    Create options.py

---
+++ worlds/gnosia/options.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from Options import Choice, OptionGroup, PerGameCommonOptions, Range, Toggle
+
+#TODO: Fill with option classes
+
+@dataclass
+class GnosiaOptions(PerGameCommonOptions):
+    #TODO: Make this
+
+option_groups = [
+
+]
+
+option_presets = [
+
+]
